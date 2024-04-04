5bc97ec5260c213df269f8d31d9d50caf9b8450a
MdeMorcilla <150717295+MdeMorcilla@users.noreply.github.com>
2024-04-04 20:11:37 +0200

    Javadoc_Videojuego
    javadoc comentado del videojuego rpg

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"VideojuegoRPG","l":"Enemigo"},{"p":"VideojuegoRPG","l":"Inventario"},{"p":"VideojuegoRPG","l":"Jugador"},{"p":"VideojuegoRPG","l":"Main"}];updateSearchResults();
